37407f68625c17e2bcbb0fd5c121aeb08077c431
Administrator <lpsddz@126.com>
2018-02-08 23:19:08 +0800

    新建类文件 新建list和类11

--- 11/interface_info_res.py
+++ 11/interface_info_res.py
@@ -1,12 +1,12 @@
 import os
-
+from interfaceinfo import InterfaceInfo as info
 #过滤sql文件中的表名
-def table_name(table_string):
+def table_name(table_string,str01 = "ODS_",str02 = "ods_"):
     if "--" in table_string:
         pass
     elif "<!" in table_string:
         pass
-    elif "ODS_" in table_string or "ods_" in table_string:
+    elif str01 in table_string or str02 in table_string:
         pass
 #过滤Hive库中含有date的字段
 def get_date_string(colu_string):
@@ -33,6 +33,28 @@ def ods_name(path):
     s = str("cat " + path)
     lis = os.popen(s).readlines()
 
+#处理sql文件中的业务主键
 
-
-
+# 读取csv文件并将文件进行切分
+def read_csv_file(csv_path):
+    try:
+        with open("interface.csv", "r") as rf:
+            csv = rf.readline().strip().split(",")
+            info.system_name = csv[0]
+            info.sub_system_code = csv[1]
+            info.interface_name = csv[2]
+            info.table_name_cn = csv[3]
+            info.job_name = csv[4]
+            info.data_cycle = csv[5]
+            info.downstream_job = csv[6]
+            info.sql_script = csv[7]
+            info.ods_table_name = csv[8]
+            info.table_type = csv[9]
+            info.primary_key = csv[10]
+            info.business_date = csv[11]
+            info.data_start_date = csv[12]
+            info.data_end_date = csv[13]
+            info.leader_name = csv[14]
+            info.remark = csv[15]
+    except IOError as ioe:
+        print("File is read error: " + str(ioe))
