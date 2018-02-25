7766e00b423f29a3e735042df69cc6033d626c73
Li Peng <lpsddz@126.com>
2018-02-25 11:16:08 +0800

    更新（2018-02-06）

--- 09/webapp/cgi-bin/athletemodel.py
+++ 09/webapp/cgi-bin/athletemodel.py
@@ -1,35 +1,35 @@
+import sqlite3
+db_name = 'coachdata.sqlite'
 
-import pickle
+def get_names_from_store():
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    results = cursor.execute("""SELECT name FROM athletes""")
+    response = [row[0] for row in results.fetchall()]
+    connection.close()
+    return(response)
 
-from athletelist import AthleteList
+def get_namesID_from_store():
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    resultes = cursor.execute("""SELECT name,id FROM athletes""")
+    response = resultes.fetchall()
+    connection.close()
+    return(response)
 
-def get_coach_data(filename):
-    try:
-        with open(filename) as f:
-            data = f.readline()
-        templ = data.strip().split(',')
-        return(AthleteList(templ.pop(0), templ.pop(0), templ))
-    except IOError as ioerr:
-        print('File error (get_coach_data): ' + str(ioerr))
-        return(None)
+def get_athlete_from_id(athlete_id):
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    results = cursor.execute("""SELECT name,dob FROM athletes WHERE id = ?""",(athlete_id,))
+    (name,dob) = results.fetchone()
+    results = cursor.execute("""SELECT value FROM timing_data WHERE athlete_id = ?""",(athlete_id,))
+    data = [row[0] for row in results]
+    response = {
+                "Name":name,
+                "DOB":dob,
+                "data":data,
+                "top3":data[0:3]
+                }
+    connection.close()
+    return(response)
 
-def put_to_store(files_list):
-    all_athletes = {}
-    for each_file in files_list:
-        ath = get_coach_data(each_file)
-        all_athletes[ath.name] = ath
-    try:
-        with open('athletes.pickle', 'wb') as athf:
-            pickle.dump(all_athletes, athf)
-    except IOError as ioerr:
-        print('File error (put_and_store): ' + str(ioerr))
-    return(all_athletes)
-
-def get_from_store():
-    all_athletes = {}
-    try:
-        with open('athletes.pickle', 'rb') as athf:
-            all_athletes = pickle.load(athf)
-    except IOError as ioerr:
-        print('File error (get_from_store): ' + str(ioerr))
-    return(all_athletes)
